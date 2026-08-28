538d740a1506c870bbd60f74b5ddaf1b5eb0d9b5
virenbobhate <virunbobhate123@gmail.com>
2026-08-28 15:56:31 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,33 @@
-const CACHE_NAME = "my-diary-v1";
+const CACHE_NAME = "my-diary-v2";
 
 const FILES = [
-    "./",
-    "./index.html",
-    "./manifest.json"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.svg"
 ];
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES))
-    );
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
